fabc363b76e6887767801606d5935000029cf475
Borovskoy Aleksey <gladiatormahotina@yandex.ru>
2022-10-21 18:19:15 +0700

    complete first and second tasks blocks

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import first  # импорт файла для для блока заданий по "Переменные и простые типы данных в Python"
+import first  # импорт файла для блока заданий по теме "Переменные и простые типы данных в Python"
+import second #импорт файла для блока заданий по теме "Операторы и выражения в Python"
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     first.helloPythonTask()  # Выполнение задачи Welcome to Python
     first.titleTask("hello world")  # Выполнение задачи Заголовок
     first.amountFormatTask(100500.157)  # Выполнение задачи Форматированный вывод денежной суммы
-
+    second.sumOfThreeRandNum()  # выполнение задачи Среднее
+    second.division()
+    second.rounding(14.721)
--- venv/first.py
+++ venv/first.py
@@ -5,14 +5,17 @@ import os
 
 #Решил реализовать функцию так, чтобы использовалось имя учётной записи компьютера
 def helloPythonTask():
+    print("-----Задача Welcome to Python-----")
     name = os.getlogin() #  получение имени учётной записи
-    print(f'Hello {name}! You just delved into Python. Great start!') # вывод нужного текста
+    print(f'Hello {name}! You just delved into Python. Great start!\n') # вывод нужного текста
 
 #Функция решения задачи "Заголовок"
 def titleTask(text):
-    print(text.title())
+    print("-----Задача Заголовок-----")
+    print("Заголовок:",text.title(),"\n")
 
 #Функция решения задачи "Форматированный вывод денежной суммы"
 def amountFormatTask(amount):
+    print("-----Форматированный вывод денежной суммы-----")
     amountFormat = "{:.2f}".format(amount)
-    print(amountFormat)
+    print("Форматированный вывод:",amountFormat,"\n")
