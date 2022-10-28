84963ae721b230f3f8e1073bacabe651e721e15e
Borovskoy Aleksey <gladiatormahotina@yandex.ru>
2022-10-28 15:49:00 +0700

    add junior and junior+ tasks

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-import first  # импорт файла для блока заданий по теме "Переменные и простые типы данных в Python"
-import second #импорт файла для блока заданий по теме "Операторы и выражения в Python"
+# import first  # импорт файла для блока заданий по теме "Переменные и простые типы данных в Python"
+# import second #импорт файла для блока заданий по теме "Операторы и выражения в Python"
+import junior
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    first.helloPythonTask()  # Выполнение задачи Welcome to Python
-    first.titleTask("hello world")  # Выполнение задачи Заголовок
-    first.amountFormatTask(100500.157)  # Выполнение задачи Форматированный вывод денежной суммы
-    second.sumOfThreeRandNum()  # выполнение задачи Среднее
-    second.division()  # Выполнение задачи Деление и ещё раз деление
-    second.rounding(14.721)  # Выполнение задачи округление
-    first.pythonArtN(10)  # Выполнение задачи python Art для буквы N
-    first.carpet(11,'HOME SWEET HOME') # Выполнение задачи дизайнер ковриков
+    print(junior.multiply(999))
+    print(junior.reverse(1563847412))
+    print(junior.threeWords("1 2 3 4"))
+    print(junior.leftReplace(["bright aright", "ok"]))
+    print(junior.mean([3, 6, 20, 99, 10, 15]))
