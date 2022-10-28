7b6fd46d64c2ca36c8fe71636a4c0705a8e84cfd
Borovskoy Aleksey <gladiatormahotina@yandex.ru>
2022-10-28 16:42:07 +0700

    prepare branch for part 6

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-import first  # импорт файла для блока заданий по теме "Переменные и простые типы данных в Python"
-import second #импорт файла для блока заданий по теме "Операторы и выражения в Python"
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    first.helloPythonTask()  # Выполнение задачи Welcome to Python
-    first.titleTask("hello world")  # Выполнение задачи Заголовок
-    first.amountFormatTask(100500.157)  # Выполнение задачи Форматированный вывод денежной суммы
-    second.sumOfThreeRandNum()  # выполнение задачи Среднее
-    second.division()  # Выполнение задачи Деление и ещё раз деление
-    second.rounding(14.721)  # Выполнение задачи округление
-    first.pythonArtN(3)  # Выполнение задачи python Art для буквы N
-    first.carpet(11,'HOME SWEET HOME') # Выполнение задачи дизайнер ковриков
