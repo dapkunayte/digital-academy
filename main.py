37f314aa79b2c7b16019a21c48b0ed8bae21947b
Borovskoy Aleksey <gladiatormahotina@yandex.ru>
2022-10-28 20:21:12 +0700

    make output in main and striped word task

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import part_6
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(part_6.lastEven([1,3,5]))
-    print(part_6.maxMin([5,-5]))
-    print(part_6.smartSort([-20,-5,10,15]))
+    print("Результат: ",part_6.lastEven([1,3,5]))
+    print("Разница между максимальным и минимальным числом в списке: ",part_6.maxMin([5,-5]))
+    print("Результат сортировки: ", part_6.smartSort([-20,-5,10,15]))
