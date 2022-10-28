33b480979478a2adc65c085fa94e4eac6d541d08
Borovskoy Aleksey <gladiatormahotina@yandex.ru>
2022-10-28 16:33:29 +0700

    add part1 tasks

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import part_3  # импорт файла для блока заданий по теме "Переменные и простые типы данных в Python"
+import part_4  # импорт файла для блока заданий по теме "Переменные и простые типы данных в Python"
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    part_3.helloPythonTask()  # Выполнение задачи Welcome to Python
-    part_3.titleTask("hello world")  # Выполнение задачи Заголовок
-    part_3.amountFormatTask(100500.157)  # Выполнение задачи Форматированный вывод денежной суммы
-    part_3.pythonArtN(3)  # Выполнение задачи python Art для буквы N
-    part_3.carpet(11,'HOME SWEET HOME') # Выполнение задачи дизайнер ковриков
+    part_4.sumOfThreeRandNum()  # выполнение задачи Среднее
+    part_4.division()  # Выполнение задачи Деление и ещё раз деление
+    part_4.rounding(14.721)  # Выполнение задачи округление
 
 
