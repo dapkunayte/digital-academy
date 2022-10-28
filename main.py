25d7c039a5e58cbcae4bd412177c49005bc7029e
Borovskoy Aleksey <gladiatormahotina@yandex.ru>
2022-10-28 16:15:37 +0700

    add tasks in part 5

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # import first  # импорт файла для блока заданий по теме "Переменные и простые типы данных в Python"
 # import second #импорт файла для блока заданий по теме "Операторы и выражения в Python"
-import junior
+#import junior
+import part_5
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(junior.multiply(999))
-    print(junior.reverse(1563847412))
-    print(junior.threeWords("1 2 3 4"))
-    print(junior.leftReplace(["bright aright", "ok"]))
-    print(junior.mean([3, 6, 20, 99, 10, 15]))
+    print(part_5.fizzBuzz(15))
+    print(part_5.estimateNum(24))
+    print(part_5.sequence(9))
+    print(part_5.secterMessage("How are you? Eh, ok. Low or Lower? Ohhh."))
