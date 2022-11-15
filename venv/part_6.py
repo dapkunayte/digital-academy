86055dcdbb4fb170e64809aae73e6e2fda398e00
Borovskoy Aleksey <gladiatormahotina@yandex.ru>
2022-11-15 18:12:55 +0700

    fix naming

--- venv/part_6.py
+++ venv/part_6.py
@@ -1,22 +1,22 @@
 # Последний с четными
-def lastEven(list)->int:
+def last_even(list)->int:
     if len(list) < 1:
         return 0
     sum=0
-    for i,x in enumerate(list):
+    for i,value in enumerate(list):
         if i%2==0:
-            sum+=x
+            sum+=value
     return sum*list[len(list)-1]
 
 #Max-min
-def maxMin(list)->float:
+def max_min(list)->float:
     if len(list) < 1:
         return 0
     else:
-        return round(max(list)-min(list),3)
+        return round(max(list)-min(list),3) # округление до трёх
 
 # Умная сортировка
-def smartSort(list):
+def smart_sort(list):
     for i in range(len(list)):
         for j in range(len(list)):
             if abs(list[i])<abs(list[j]):
