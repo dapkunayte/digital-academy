162e678da5c5b05a37330b43c5d06dcc979045e1
Borovskoy Aleksey <gladiatormahotina@yandex.ru>
2022-11-22 12:20:43 +0700

    fix naming x2

--- venv/junior_part_8.py
+++ venv/junior_part_8.py
@@ -37,8 +37,6 @@ def decrypter(crypt) -> str:
     return ''.join(first_iter+second_iter+third_iter+fourth_iter)
 
 # возвраст приведений
-
-
 def ghost_age(transparency) -> int:
     max_trasparency = 10000
     age = 0
@@ -56,10 +54,5 @@ def isPerfectSquare(x):
     s = int(math.sqrt(x))
     return s * s == x
 
-    # Returns true if n is a Fibonacci Number, else false
-
-
 def isFibonacci(n):
-    # n is Fibonacci if one of 5*n*n + 4 or 5*n*n - 4 or both
-    # is a perfect square
     return isPerfectSquare(5 * n * n + 4) or isPerfectSquare(5 * n * n - 4)
