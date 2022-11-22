3e022e798e7da100682694e2a6acc63d7a7e23bf
Borovskoy Aleksey <gladiatormahotina@yandex.ru>
2022-11-22 10:56:00 +0700

    complete junior- tasks

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-import part_5
+import part_8
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(part_5.fizz_buzz(15))
-    print(part_5.estimate_num(24))
-    print(part_5.sequence(9))
-    print(part_5.secter_message("How are you? Eh, ok. Low or Lower? Ohhh."))
+    # не уникальные элементы
+    number_list = [10, 9, 10, 10, 9, 8]
+    print(part_8.not_uniq_elements(number_list))
 
+    # перестановки
+    x, y, z, n = 1, 1, 1, 2
+    print(part_8.my_permutations(x, y, z, n))
+
+    # удвоенные нечётные
+    n = 5
+    print(part_8.sqr_odd_numbers(n))
