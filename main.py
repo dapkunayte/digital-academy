53187dcf6455101492160e870678b1aacf39e670
Borovskoy Aleksey <gladiatormahotina@yandex.ru>
2022-11-25 16:23:47 +0700

    complete tasks in part 9

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
-import part_8
+import part_9
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # не уникальные элементы
-    number_list = [10, 9, 10, 10, 9, 8]
-    print(part_8.not_uniq_elements(number_list))
+    # Список из числа
+    number = 123
+    print(part_9.reverse_number(number))
 
-    # перестановки
-    x, y, z, n = 1, 1, 1, 2
-    print(part_8.my_permutations(x, y, z, n))
+    # Палиндром
+    string_false = 'hi'
+    string_true = 404
+    print(part_9.is_palindrome(string_false))
+    print(part_9.is_palindrome(string_true))
 
-    # удвоенные нечётные
-    n = 5
-    print(part_8.sqr_odd_numbers(n))
+    #Деканат
+    students_info = [['Кривошеин Илья Андреевич', 2000, '759-B', [5, 3, 4, 3, 3]],
+                     ['Петров Илья Андреевич', 2001, '759-С', [5, 3, 4, 4, 4]],
+                     ['Смирнов Илья Андреевич', 2002, '759-B', [5, 3, 4, 3, 4]],
+                     ['Дметров Илья Андреевич', 2000, '759-С', [5, 5, 4, 4, 4]]]
+    sorted_names = 'sorted_names'
+    sorted_ages = 'sorted_ages'
+    best_marks_in_group = 'best_marks_in_group'
+    mean_marks = 'mean_marks'
+    print("Имена студентов в алфавитном порядке:", part_9.dean_office(students_info, sorted_names))
+    younger_student, older_student = part_9.dean_office(students_info, sorted_ages)
+    print("Самый младший студент:", younger_student, "Самый старший студент:", older_student)
+    print("Лучшая успеваемость по группам", part_9.dean_office(students_info,best_marks_in_group))
+    print("Средний балл по каждой группе по каждому предмету", part_9.dean_office(students_info, mean_marks))
+
---
+++ venv/part_9.py
@@ -0,0 +1,59 @@
+from collections import defaultdict
+def reverse_number(number)->list:
+    str_number = str(number)
+    return list(str_number)[::-1] # используем срез для разворота списка
+
+def is_palindrome(word)->bool:
+    if type(word)!=str: # проверка на тип входных данных (для обработки чисел)
+        word = str(word)
+    if word[::-1] == word: # срез для разворота строки и сравнение с оригиналом
+        return True
+    else:
+        return False
+
+def dean_office(students_info, need_to):
+    # имена в алфавитном порядке
+    if need_to == 'sorted_names':
+        names = []
+        for student in students_info:
+            name = student[0]
+            names.append(name)
+        return sorted(names)
+
+    # cамый младший и старший
+    if need_to == 'sorted_ages':
+        ages = {}
+        for student in students_info:
+            name,age = student[0],student[1]
+            ages[name] = age
+        sorted_age = sorted(ages.items())
+        return sorted_age[0], sorted_age[len(sorted_age)-1] # максимум
+
+    # лучший студент
+    if need_to == 'best_marks_in_group':
+        best_marks_in_group = {}
+        for student in students_info:
+            name,group,marks = student[0],student[2],student[3]
+            if group in best_marks_in_group:
+                if best_marks_in_group[group][1] < sum(marks) / len(marks): # best_marks_in_group[group][1] - 1 отвечает за местоположение средней оценки в группе
+                    best_marks_in_group[group]=[name,sum(marks) / len(marks)]
+            else:
+                best_marks_in_group[group] = [name, sum(marks) / len(marks)]
+        return best_marks_in_group
+
+    # средний балл по всем предметам для каждой группы
+
+    if need_to == 'mean_marks':
+        marks_list = defaultdict(list)
+        for student in students_info:
+            group,marks = student[2],student[3]
+            marks_list[group].append(marks)
+        group_marks = defaultdict(list)
+        for group in marks_list:
+            i=1
+            for marks in zip(*marks_list[group]): # не особо эффективно из-за вложенного цикла, но другого решения не нашёл
+                group_marks[group].append(['Предмет '+str(i) ,sum(marks)/len(marks)])
+                i+=1
+        return dict(group_marks)
+
+
