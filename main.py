c85a0c74968ace99c892f6d060ed89f23f5ede5c
Borovskoy Aleksey <gladiatormahotina@yandex.ru>
2022-11-26 20:35:00 +0700

    complete custom max-min function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import junior
 import junior_part_7 as jp7
 import junior_part_8 as jp8
+import junior_part_9 as jp9
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -32,6 +33,24 @@ if __name__ == '__main__':
 
     # возраст приведений
     print("Возраст приведения", jp8.ghost_age(9990))
-    print(max(1, 1))
+
+    # примеры максимума и минимума
+    print(jp9.minimum(3, 2))
+    print(jp9.maximum(3, 2))
+
+    print(jp9.minimum([1, 2, 0, 3, 4]))
+    print(jp9.maximum([1, 2, 0, 3, 4]))
+
+    print(jp9.minimum("hello"))
+    print(jp9.maximum("hello"))
+
+    print(jp9.minimum(2.2, 5.6, 5.9, key=int))
+    print(jp9.maximum(2.2, 5.6, 5.9, key=int))
+
+    print(jp9.minimum([[1,2], [3, 4], [9, 0]], key=lambda x: x[1]))
+    print(jp9.maximum([[1,2], [3, 4], [9, 0]], key=lambda x: x[1]))
+
+
+
 
 
--- venv/junior_part_9.py
+++ venv/junior_part_9.py
@@ -1,4 +1,67 @@
-def maximum(a,b=None):
-    if b!=None:
-        if a > b:
-            return a
+class bcolors: # при помощи этого можно реализовать вывод текста в консоль с другим цветом (использовал FAIL для ошибок)
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def maximum(*variables,key=lambda x: x):
+        if len(variables)==1:
+            if hasattr(*variables,"__iter__"): # проверка, является ли переданный аргумент (когда он один) итерируемым
+                variables_dict = {}
+                for variable in list(*variables):
+                    if key(variable) not in variables_dict.keys():
+                        variables_dict[key(variable)] = variable
+                sorted_variables = sorted(variables_dict.items())
+                len_sorted_variables = len(sorted_variables)-1
+                return sorted_variables[len_sorted_variables][1]
+            else:
+                return f"{bcolors.FAIL}TypeError: {type(*variables)} object is not iterable"
+        else:
+            variables_dict = {}
+            for variable in list(variables):
+                if key(variable) not in variables_dict.keys():
+                    variables_dict[key(variable)] = variable
+            sorted_variables = sorted(variables_dict.items())
+            len_sorted_variables = len(sorted_variables) - 1
+            return sorted_variables[len_sorted_variables][1]
+
+
+def minimum(*variables, key=lambda x: x):
+    if len(variables) == 1:
+        if hasattr(*variables, "__iter__"):  # проверка, является ли переданный аргумент (когда он один) итерируемым
+            variables_dict = {}
+            for variable in list(*variables):
+                if key(variable) not in variables_dict.keys():
+                    variables_dict[key(variable)] = variable
+            sorted_variables = sorted(variables_dict.items())
+            len_sorted_variables = len(sorted_variables) - 1
+            return sorted_variables[0][1]
+        else:
+            return f"{bcolors.FAIL}TypeError: {type(*variables)} object is not iterable"
+    else:
+        variables_dict = {}
+        for variable in list(variables):
+            if key(variable) not in variables_dict.keys():
+                variables_dict[key(variable)] = variable
+        sorted_variables = sorted(variables_dict.items())
+        len_sorted_variables = len(sorted_variables) - 1
+        return sorted_variables[0][1]
+"""
+        a_with_key = a
+        if key != None: # если ключ был передан
+            try: # попытка применить функцию из ключа на переданный параметр
+                a_with_key = [key(x) for x in a_with_key]
+            except TypeError:
+                return f"{bcolors.FAIL}TypeError: {type(a)} object cant't use with this key" # возврат ошибки, если нельзя применить функцию
+        try: # проверка является ли переданный объект итерируемым
+            sorted_a = sorted(a_with_key) # по условию sorted не запрещено было использовать
+            max_val_index = len(sorted_a)-1
+            return sorted_a[max_val_index]
+        except TypeError:
+            return f"{bcolors.FAIL}TypeError: {type(a)} object is not iterable"
+"""
